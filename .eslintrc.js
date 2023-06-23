f7dcb5a422136bfed09a1027e336bf09666d97a0
theroamingcode <thebilljustin@gmail.com>
2023-06-23 22:03:07 +0800

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,37 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        "plugin:vue/vue3-essential",
+        "@vue/standard"
+    ],
+    parserOptions: {
+        parser: "@babel/eslint-parser"
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "off" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "off" : "off",
+        quotes: ["error", "double"],
+        indent: ["error", 4],
+        "no-tabs": 0,
+        "brace-style": ["error", "stroustrup"],
+        "no-mixed-spaces-and-tabs": 0,
+        "no-unused-vars": "off",
+        "vue/multi-word-component-names": "off",
+        "vue/valid-template-root": "off", // remove this,
+        "vue/no-mutating-props": "off" // update and remove tis warn
+    },
+    overrides: [
+        {
+            files: [
+                "**/__tests__/*.{j,t}s?(x)",
+                "**/tests/unit/**/*.spec.{j,t}s?(x)"
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
 }
